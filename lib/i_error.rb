493651847e5aa3486ee40ee29b85d44780256283
IwaiMiku <arashi5.miku@icloud.com>
2023-02-21 12:14:43 +0900

    add i_error.rb

--- lib/i_error.rb
+++ lib/i_error.rb
@@ -6,34 +6,27 @@ require_relative "i_error/version"
 
 module IError
   class Error < StandardError; end
-  # Your code goes here...
   
-  def from_file(file='tmp.txt')
-    File.readlines('tmp.txt').each do |line|
-      input_string = "NoMethodError: undefined method chomp' for #<MatchData \"# head1\" 1:\"#\" 2:\"head1\">"
-      puts input_string
-      p result = command_line("trans :ja #{input_string}")
-      p line.match(/(.+?)\(LoadError\)/) #正規表現
-      puts "ruby: LoadErrorです. hoge.rb というファイルあるいはディレクトリがありません."
-    end
-  end
-
   lines = STDIN.readlines
-    client = Google::Cloud::Translate::V2.new(
-      key: ENV["GOOGLE_TRANSLATE_KEY"]
-    )
-  #contents = "environment.rb:5:in 'translate': wrong number of arguments (given 0, expected 3) (ArgumentError)
-  #from environment.rb:17:in '<main>'"
-    p lines
+  client = Google::Cloud::Translate::V2.new(
+    key: ENV["GOOGLE_TRANSLATE_KEY"]
+  )
+  
+  puts lines
   lines.each do |line|
-    p "hoge"
-    translation = client.translate line, to: "ja"
+    translation = client.translate lines, to: "ja"
 
     puts translation #=> Salve mundi!
-
-    translation.from #=> "en"
-    translation.origin #=> "Hello world!"
-    translation.to #=> "la"
-    translation.text #=> "Salve mundi!"
   end
 end
+
+  client = Google::Cloud::Translate::V2.new(
+    key: ENV["GOOGLE_TRANSLATE_KEY"]
+  )
+  contents = "environment.rb:5:in 'translate':
+  wrong number of arguments (given 0, expected 3) (ArgumentError)
+  from environment.rb:17:in '<main>'"
+
+  translation = client.translate contents, to: "ja"
+
+  puts translation #=> Salve mundi!
